cbce8581fa3cab829ee86f4e8654c998fb4ebd04
Joswal <princejoswal@gmail.com>
2019-07-05 23:06:49 +0100

    completed the user register,login api and fixed bugs conatined in the user apis

--- routes/loans.js
+++ routes/loans.js
@@ -1,23 +1,31 @@
-const Loan = require('../models/loanModel');
-const express = require('express');
-const router = express.Router();
+const Loan = require("../models/loanModel")
+const express = require("express")
+const router = express.Router()
 
 let loans = []
 
-const renmoneyloan = new Loan(1, "Ren Money", "Salary earners discounted loans", 3, 50000, "1.5 years")
+const renmoneyloan = new Loan(
+  1,
+  "Ren Money",
+  "Salary earners discounted loans",
+  3,
+  50000,
+  "1.5 years"
+)
 const easyLoan = new Loan(2, "KiaKia", "Easy Small loan", 5, 5000, "3 months")
 
-router.get('/', async (req, res) => {
-  loans.push(renmoneyloan);
-  loans.push(easyLoan);
-  res.status(200).send(loans);
-});
+router.get("/", async (req, res) => {
+  loans.push(renmoneyloan)
+  loans.push(easyLoan)
+  res.status(200).send(loans)
+  console.log(loans)
+})
 
-router.post('/apply', async (req, res) => {
+router.post("/apply", async (req, res) => {
   const {
     error
-  } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  } = validate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
   const loan = new Loan({
     title: req.body.title,
@@ -27,11 +35,10 @@ router.post('/apply', async (req, res) => {
     },
     numberInStock: req.body.numberInStock,
     dailyRequestRate: req.body.dailyRequestRate
-  });
-  await loan.save();
+  })
+  await loan.save()
 
-  res.send(loan);
-});
+  res.send(loan)
+})
 
-
-module.exports = router;
+module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,42 +1,54 @@
-const auth = require('../middleware/auth');
-const _ = require('lodash');
+const auth = require("../middleware/auth")
+const _ = require("lodash")
 const {
   User,
   validate
-} = require('../models/userModel');
-const express = require('express');
-const router = express.Router();
+} = require("../models/userModel")
+const express = require("express")
+const router = express.Router()
 
 const users = []
 
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   const {
     error
-  } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  } = validate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
-  const found = users.find((person) => person.email == req.body.email);
-  if (found) return res.status(400).send("user already registered, please register another user");
-  console.log(found);
+  const found = users.find((person) => person.email == req.body.email || person.username == req.body.username || person.phoneNumber == req.body.phoneNumber)
+  if (found)
+    return res
+      .status(400)
+      .send("user already registered, please register another user")
 
-  if (req.body.password != req.body.verifyPassword) return res.status(400).send("passwords do not match, please check password and verify password");
+  if (req.body.password != req.body.verifyPassword)
+    return res
+      .status(400)
+      .send("passwords do not match, please check password and verify password")
 
-  user = new User(_.pick(req.body, ['name', 'username', 'email', 'password', 'verifyPassword', 'phoneNumber']));
-  console.log(user);
-  users.push(user);
-  console.log(users);
-  res.status(201).send("user succesfully registered");
-});
+  let user = new User(req.body.name, req.body.username, req.body.email, req.body.password, req.body.verifyPassword, req.body.phoneNumber);
+  users.push(user)
+  console.log(users)
+  res.status(201).send("user succesfully registered")
+})
 
-router.post('/login', async (req, res) => {
-  console.log(users);
-  const registered = users.find((person) => person.username == req.body.username);
-  console.log(registered);
-  if (!registered) return res.status(400).send("user not yet registered, please register first");
 
-  if (registered.password != req.body.password) return res.status(400).send("wrong login details, please try again");
+router.post("/login", async (req, res) => {
+  console.log(users)
+  if (!(req.body.email || req.body.username) && req.body.password) return res.status(400).send("incomplete login details, please try again")
+  const registered = users.find((person) => person.username == req.body.username || person.email == req.body.email)
+  console.log(registered)
+  if (!registered)
+    return res
+      .status(400)
+      .send("user not yet registered, please register first")
 
-  res.header(201).send(_.pick(registered, ['name', 'username', 'email', 'phoneNumber']));
-});
+  if (registered.password != req.body.password)
+    return res.status(400).send("wrong login details, please try again")
 
-module.exports = router;
+  res
+    .header(200)
+    .send(_.pick(registered, ["name", "username", "email", "phoneNumber"]))
+})
+
+module.exports = router
