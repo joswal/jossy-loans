07a6394a31096f5e7afecb6e26d5513915d2e648
Joswal <princejoswal@gmail.com>
2019-07-05 02:30:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require('express')();
+require('./startup/routes')(app);
+require('./startup/validation')();
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => console.log(`Listening on port ${port}`));
+
+module.exports = server;
