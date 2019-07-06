cc3a39243f032e4c743bb75995a1608799665963
Joswal <princejoswal@gmail.com>
2019-07-06 07:51:17 +0100

    added some tests suites

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -1,17 +1,30 @@
-const {User} = require('../../../models/userModel');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const mongoose = require('mongoose');
+const {
+  User,
+  validate
+} = require('../../../models/userModel');
 
-describe('user.generateAuthToken', () => {
-  it('should return a valid JWT', () => {
-    const payload = { 
-      _id: new mongoose.Types.ObjectId().toHexString(), 
-      isAdmin: true 
-    };
-    const user = new User(payload);
-    const token = user.generateAuthToken();
-    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-    expect(decoded).toMatchObject(payload);
+describe('user.authenticateUser', () => {
+  it('should change value of user.isLoggedIn to true', () => {
+    const user = new User("wale", "joswal", "princejoswal@gmail.com", "123456", "123456", "08062467783", "false");
+    user.authenticateUser();
+    expect(user.isLoggedIn).toBe(true);
+  });
+});
+
+describe('validate User', () => {
+  it('it should validate the user details and return no error if all details are correct ', () => {
+    const user = new User("wale", "joswal", "princejoswal@gmail.com", "123456", "123456", "08062467783");
+    const {
+      error
+    } = validate(user);
+    expect(error).toBe(false);
+  });
+
+  it('it should set status as 400 and return appropriate message if any of the details is correct ', () => {
+    const user = new User("wale", "joswal", "princejoswal@gmail.com", "123456", "123456", "08062467783");
+    const {
+      error
+    } = validate(user);
+    expect(user.isLoggedIn).toBe(true);
   });
 });
